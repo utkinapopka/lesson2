c59a9c94786c937a7d6ba5b65832e1990f244288
utkinapopka <utkinapopka@gmail.com>
2024-12-03 00:02:40 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,6 @@
+hw_count=12;
+hw_hours=1.5;
+course='Python';
+lesson_time= (1.5*60)/12;
+print('Курс:',course, ",",'всего задач:', hw_count, ",", 'среднее время выполнения:',lesson_time, 'минут.');
+print('Курс: '+ str(course)  + ", "+'всего задач:'+ str(hw_count)+ ","+ ' среднее время выполнения: '+str(lesson_time)+ ' минут.');
